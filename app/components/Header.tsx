b1ff3555279a52d2771097ee1e2122290aeff21b
Daniel Fraga <dan.fraga33@gmail.com>
2025-03-28 21:07:54 +1100

    fix: add github link to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -34,8 +34,10 @@ const Header = () => {
             <Search className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-muted-foreground" />
           </div>
 
-          <Button variant="ghost" size="icon">
-            <Github className="h-4 w-4" />
+          <Button variant="ghost" size="icon" asChild>
+            <a href="https://github.com/Danfraga33" target="_blank">
+              <Github className="h-4 w-4" />
+            </a>
           </Button>
         </div>
       </div>
